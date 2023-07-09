21628a5b5869948a442878da2375f9625a9134aa
220v-K <jwlee010222@gmail.com>
2023-07-09 14:58:10 +0900

    first commit

---
+++ SixTech/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SixTech
+//
+//  Created by 이재원 on 2023/07/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SixTech/SixTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  SixTechApp.swift
+//  SixTech
+//
+//  Created by 이재원 on 2023/07/09.
+//
+
+import SwiftUI
+
+@main
+struct SixTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
