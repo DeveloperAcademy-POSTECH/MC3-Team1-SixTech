e402c114afd8da128713a1d80df5022ea6c39b35
Musk <yuiop1029@naver.com>
2023-07-15 14:22:05 +0900

    #4 <fix> : Fix enum for working

--- SixTech/Extension/FontExtension.swift
+++ SixTech/Extension/FontExtension.swift
@@ -10,12 +10,12 @@ import SwiftUI
 
 extension Font {
     enum Jamsil: String {
-        case thin = "TheJamsilOTF1Thin.otf"
-        case light = "TheJamsilOTF2Light.otf"
-        case regular = "TheJamsilOTF3Regular.otf"
-        case medium = "TheJamsilOTF4Medium.otf"
-        case bold = "TheJamsilOTF5Bold.otf"
-        case extraBold = "TheJamsilOTF6ExtraBold.otf"
+        case thin = "TheJamsilOTF1Thin"
+        case light = "TheJamsilOTF2Light"
+        case regular = "TheJamsilOTF3Regular"
+        case medium = "TheJamsilOTF4Medium"
+        case bold = "TheJamsilOTF5Bold"
+        case extraBold = "TheJamsilOTF6ExtraBold"
         
         func font(size: CGFloat) -> Font {
             return Font.custom(rawValue, size: size)
