b151f82af593dd5473553dbbdb4147b608eb8441
220v-K <jwlee010222@gmail.com>
2023-07-26 04:27:57 +0900

    <add>: WatchOS Proj

---
+++ LetPlogWatch Watch App/LetPlogWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetPlogWatchApp.swift
+//  LetPlogWatch Watch App
+//
+//  Created by 이재원 on 2023/07/26.
+//
+
+import SwiftUI
+
+@main
+struct LetPlogWatch: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
