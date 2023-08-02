cf29a3fd125bc13bb421823df3c3c574c9c8b9db
220v-K <jwlee010222@gmail.com>
2023-08-02 18:59:09 +0900

    <add>: Watch Main View

--- PlogWatch Watch App/PlogWatchApp.swift
+++ PlogWatch Watch App/PlogWatchApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PlogWatch_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WatchMainView()
         }
     }
 }
---
+++ PlogWatch Watch App/WatchMainView.swift
@@ -0,0 +1,20 @@
+//
+//  WatchMainView.swift
+//  PlogWatch Watch App
+//
+//  Created by 이재원 on 2023/08/02.
+//
+
+import SwiftUI
+
+struct WatchMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WatchMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        WatchMainView()
+    }
+}
